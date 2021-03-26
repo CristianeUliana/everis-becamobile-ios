3905d69d3d69129a7e2d05ea3c9235cbab528a45
Cristiane Goncalves Uliana <cristianeguliana@gmail.com>
2021-03-26 17:38:08 -0300

    criacao da collection view, celulas sendo populadas com as imagens dos filmes

--- FilmeApp/FilmeApp/Home/Controller/ViewController.swift
+++ FilmeApp/FilmeApp/Home/Controller/ViewController.swift
@@ -10,65 +10,59 @@ import UIKit
 import Alamofire
 import AlamofireImage
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UICollectionViewDataSource, UICollectionViewDelegate {
 
+    // MARK: - IBOutlets
+    
+    @IBOutlet weak var colecaoFilmes: UICollectionView!
+    
+    
+    // MARK: - Variáveis
     
     var listaDeFilmes: [Filme] = []
     
-    //teste requisição API
     
+    // MARK: - LifeCycle
     
-   // https://api.themoviedb.org/3/trending/all/week?api_key=6f6ac16c48e86c7c1e800a462c1c1c4b&language=pt-BR/v9XwEXYWpxt2rpkmFBiQ1zeyKUy.jpg
-    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        testeRequisicaoApi()
+        colecaoFilmes.delegate = self
+        colecaoFilmes.dataSource = self
+    }
 
+    // MARK: - Requisição API
     
     func testeRequisicaoApi() {
         Alamofire.request("https://api.themoviedb.org/3/trending/all/week?api_key=6f6ac16c48e86c7c1e800a462c1c1c4b&language=pt-BR", method: .get).responseJSON { (response) in
             switch response.result {
                 case .success:
-                    
-//                    var caminhoDoFilme:[Any] = []
-//                    var movieId:[Any] = []
-//
                     if let resposta = response.result.value as? Dictionary<String,Any> {
                         guard let filmes = resposta["results"] as? Array<Dictionary<String, Any>> else {return}
                         
                         for filme in filmes {
-                            
-                           
-                            
                             self.salvaFilme(filme)
-                            
-                            
-//                            guard let id = filme["id"] else {return}
-//                            guard let caminho = filme["poster_path"] else {return}
-//                            caminhoDoFilme.append(caminho)
-//                            movieId.append(id)
                         }
+//                        print(self.listaDeFilmes[0].titulo)
+//                        print(self.listaDeFilmes[0].caminhoPoster)
+//                        print(self.listaDeFilmes[11].titulo)
+//                        print(self.listaDeFilmes[11].caminhoPoster)
                         
-                        print(self.listaDeFilmes[0].titulo)
-                        print(self.listaDeFilmes[0].caminhoPoster)
-                        print(self.listaDeFilmes[11].titulo)
-                        print(self.listaDeFilmes[11].caminhoPoster)
-                        
-//
-//                        print(movieId)
-//                        print(caminhoDoFilme)
+                        self.colecaoFilmes.reloadData()
                     }
-                    
                 break
             case .failure:
                 print(response.error!)
                 break
             }
         }
-        
     }
     
     
+    // MARK: - Métodos
+    
     func salvaFilme(_ dicionarioFilme: Dictionary<String,Any>) {
-        
-        
+    
         guard let id = dicionarioFilme["id"] as? Int else {return}
         guard let titulo = dicionarioFilme["title"] as? String else {return}
         guard let rating = dicionarioFilme["vote_average"] as? Double else {return}
@@ -78,28 +72,48 @@ class ViewController: UIViewController {
         
         let filme = Filme(id, titulo, rating, sinopse, caminhoPoster)
         listaDeFilmes.append(filme)
+    }
+    
+                        
+    
+    
+    // MARK: - CollectionView
+    
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
         
-       
-        
+        print(listaDeFilmes.count)
+        return self.listaDeFilmes.count
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let celulaFilme = collectionView.dequeueReusableCell(withReuseIdentifier: "cell", for: indexPath) as! FilmeCollectionViewCell
+        let filmeAtual = listaDeFilmes[indexPath.item]
+        celulaFilme.configuraCelula(filme: filmeAtual)
+        return celulaFilme
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        return UIDevice.current.userInterfaceIdiom == .phone ? CGSize(width: collectionView.bounds.width/2-20, height: 160) : CGSize(width: collectionView.bounds.width/3-20, height: 250)
     }
     
     
     
     
-                        
-                        
-                        
-//                        https://image.tmdb.org/t/p/w500
-//                        poster_path
-                    
-                        
+//    func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+//
+//
+//
+//        let pacote = listaViagens[indexPath.item]
+//        let storyboard = UIStoryboard(name: "Main", bundle: nil)
+//        let controller = storyboard.instantiateViewController(withIdentifier: "detalhes") as! DetalhesViagensViewController
+//        controller.pacoteSelecionado = pacote
+//        self.navigationController?.pushViewController(controller, animated: true)
+//    }
     
     
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        testeRequisicaoApi()
-    }
-
+    
+   
 
 }
 
--- FilmeApp/FilmeApp/Home/View/FilmeCollectionViewCell.swift
+++ FilmeApp/FilmeApp/Home/View/FilmeCollectionViewCell.swift
@@ -7,17 +7,24 @@
 //
 
 import UIKit
+import AlamofireImage
 
 class FilmeCollectionViewCell: UICollectionViewCell {
     
     
     @IBOutlet weak var celulaFilme: UIImageView!
     
-    func configuraCelula() {
+    func configuraCelula(filme:Filme) {
+        
+       guard let urlCelula = URL(string: filme.caminhoPoster) else { return }
+       
+        celulaFilme.af_setImage(withURL: urlCelula)
         
         
+        layer.borderWidth = 0.5
+        layer.borderColor = UIColor(red: 85.0/255.0, green: 85.0/255.0, blue: 85.0/255.0, alpha: 1).cgColor
+        layer.cornerRadius = 8
         
     }
-    
     
 }
