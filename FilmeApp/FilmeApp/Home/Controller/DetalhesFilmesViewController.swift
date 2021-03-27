2cc4114825dab6dfc3e36defa3e11e99337c44dd
Cristiane Goncalves Uliana <cristianeguliana@gmail.com>
2021-03-27 18:23:57 -0300

    populando informações na tela detalhes

--- FilmeApp/FilmeApp/Home/Controller/DetalhesFilmesViewController.swift
+++ FilmeApp/FilmeApp/Home/Controller/DetalhesFilmesViewController.swift
@@ -10,24 +10,22 @@ import UIKit
 
 class DetalhesFilmesViewController: UIViewController {
 
+    @IBOutlet weak var nomeFilmeLabel: UILabel!
+    @IBOutlet weak var imagemFilmeImage: UIImageView!
+    @IBOutlet weak var detalhesFilmeLabel: UILabel!
+    @IBOutlet weak var ratingFilmeLabel: UILabel!
+    
     var filmeSelecionado: Filme? = nil
     
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        if let filme = filmeSelecionado {
+            guard let urlCelula = URL(string: filme.caminhoPoster) else { return }
+            imagemFilmeImage.af_setImage(withURL: urlCelula)
+            self.nomeFilmeLabel.text = filme.titulo
+            self.detalhesFilmeLabel.text = filme.sinopse
+            self.ratingFilmeLabel.text = "\(filme.rating)"
+        }
     }
-
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
 }
