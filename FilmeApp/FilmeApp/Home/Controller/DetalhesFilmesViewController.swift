efe1df708d0b15f1a2b04116ffeacb0775328bcf
Cristiane Goncalves Uliana <cristianeguliana@gmail.com>
2021-03-27 23:32:41 -0300

    aprimoramento da tela de detalhes, inclusão de scroll view, mostrando texto completo na sinopse

--- FilmeApp/FilmeApp/Home/Controller/DetalhesFilmesViewController.swift
+++ FilmeApp/FilmeApp/Home/Controller/DetalhesFilmesViewController.swift
@@ -14,18 +14,34 @@ class DetalhesFilmesViewController: UIViewController {
     @IBOutlet weak var imagemFilmeImage: UIImageView!
     @IBOutlet weak var detalhesFilmeLabel: UILabel!
     @IBOutlet weak var ratingFilmeLabel: UILabel!
+    @IBOutlet weak var tituloOriginalLabel: UILabel!
+    @IBOutlet weak var scrollPrincipal: UIScrollView!
+    
+    
+    
+    
     
     var filmeSelecionado: Filme? = nil
     
     
     override func viewDidLoad() {
         super.viewDidLoad()
+       
+        self.scrollPrincipal.contentSize = CGSize(width: self.scrollPrincipal.frame.width, height: self.scrollPrincipal.frame.height + 320)
+        
+        
         if let filme = filmeSelecionado {
             guard let urlCelula = URL(string: filme.caminhoImagemDetalhes) else { return }
             imagemFilmeImage.af_setImage(withURL: urlCelula)
             self.nomeFilmeLabel.text = filme.titulo
             self.detalhesFilmeLabel.text = filme.sinopse
-            self.ratingFilmeLabel.text = "\(filme.rating)"
+            self.tituloOriginalLabel.text = filme.tituloOriginal
+            self.ratingFilmeLabel.text = "Rating: \(filme.rating)"
         }
+
+       
+        
+        
+        
     }
 }
